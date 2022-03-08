8e334dccbd458e7623750e5ef22df60c9991f2c2
mev <mev@PC-19-24>
2022-03-08 17:24:58 +0100

    Add project files.

---
+++ Palp2022_Vjezba/Controllers/Moj_kontroler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Palp2022_Vjezba.Controllers
+{
+    public class Moj_kontroler : Controller
+    {
+        // GET: Moj_kontroler
+        public ActionResult Index()
+        {
+            ViewBag.Lokacija = "Međimursko veleučilište u Čakovcu";
+            return View();
+        }
+
+        public ActionResult Druga()
+        {
+            ViewBag.Ustanova = "Međimursko veleučilište";
+            ViewData["Lokacija"] = "Čakovec";
+            return View();
+        }
+
+        public ActionResult Treca(string poruka, int broj = 1)
+        {
+            ViewBag.Poruka = poruka;
+            ViewBag.Broj = broj;
+            return View();
+        }
+
+        public ActionResult Cetvrta(string ime, string prezime, float godinaRodjenja)
+        {
+            ViewBag.ime = ime;
+            ViewBag.prezime = prezime;
+            ViewBag.godinaRodjenja = godinaRodjenja;
+            return View();
+        }
+    }
+}
---
+++ Palp2022_Vjezba/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Palp2022_Vjezba.MvcApplication" Language="C#" %>
